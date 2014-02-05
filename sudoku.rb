6bfaa906a217ca10a75e5deb3242eefad3b031b8
Makis Otman <makisotman@gmail.com>
2014-02-05 17:15:12 +0000

    added puzzle method, solution session plus minor tweak in css

--- sudoku.rb
+++ sudoku.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
+enable :sessions
+
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
   sudoku = Sudoku.new(seed.join)
@@ -9,8 +11,18 @@ def random_sudoku
   sudoku.to_s.chars
 end
 
+def puzzle(sudoku)
+  sudoku.map { |i| rand(1..9) < 4.1 ? 0 : i}
+end
+
 get '/' do
-  @current_solution = random_sudoku
-  puts @current_solution.inspect
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  @current_solution = puzzle(sudoku)
+  erb :index
+end
+
+get '/solution' do
+  @current_solution = session[:solution]
   erb :index
 end
